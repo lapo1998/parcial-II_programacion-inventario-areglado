5a676f71b4f1085628f7badab68abb730c2bae7f
lapo1998 <elvilapo@hotmail.com>
2025-09-06 12:54:39 -0500

    Update libro.py

--- Sistema de Gestión de Biblioteca Digital semana 12/libro.py
+++ Sistema de Gestión de Biblioteca Digital semana 12/libro.py
@@ -1,5 +1,3 @@
-# Archivo: libro.py
-
 class Libro:
     def __init__(self, titulo, autor, categoria, isbn):
         # Utiliza una tupla para el título y el autor para hacerlos inmutables.
@@ -9,4 +7,5 @@ class Libro:
 
     def __repr__(self):
         # Representación del objeto, útil para la depuración y visualización.
+
         return f"Libro(título='{self.datos[0]}', autor='{self.datos[1]}', categoría='{self.categoria}', ISBN='{self.isbn}')"
